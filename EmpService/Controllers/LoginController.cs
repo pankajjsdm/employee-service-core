573d7fbe28cd6a20d188e76f4f0545842b7db9be
Pankaj Sharma <pankajjsdm@gmail.com>
2020-05-31 15:36:04 +0530

    Adding JWT Authentication

---
+++ EmpService/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿
+using EmpService.Request;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpService.Controllers
+{
+    [Authorize]
+    [ApiController]
+   //[Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private ILoginService _loginServices;
+
+        public LoginController(ILoginService userService)
+        {
+            _loginServices = userService;
+        }
+
+        [AllowAnonymous]
+        [Route("api/login")]
+        public IActionResult Authenticate([FromBody] LoginRequest model)
+        {
+            var user = _loginServices.Authenticate(model.Username , model.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+        [HttpGet]
+        [Route("api/GetUsers")]
+        public IActionResult GetAll()
+        {
+            var users = _loginServices.GetAll();
+            return Ok(users);
+        }
+    }
+}
--- EmpService/Startup.cs
+++ EmpService/Startup.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using EmpService.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EmpService
 {
@@ -30,7 +32,31 @@ namespace EmpService
             services.AddControllers();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddDbContext<EmployeeContext>(op => op.UseSqlServer(Configuration["ConnectionString:EmpDB"]));
+
+            //const string sec = "401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
+            string secKey = Configuration["Jwt:Key"];
+            var key = Encoding.ASCII.GetBytes(secKey);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddScoped<ILoginService, LoginService>();            
             services.AddTransient(typeof(IDBRepository<Emp>), typeof(EmployeeRepository));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +66,7 @@ namespace EmpService
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseAuthentication();
             app.UseHttpsRedirection();
 
             app.UseRouting();
